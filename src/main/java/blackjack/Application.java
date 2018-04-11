520d337c5e850dd7d5ebc3d496fda8d8d344687e
Aaron Ofengender <aofengen@gmail.com>
2018-04-11 12:01:33 -0400

    hopefully deploying to heroku

--- src/main/java/blackjack/Application.java
+++ src/main/java/blackjack/Application.java
@@ -6,7 +6,10 @@ package blackjack;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 //import org.springframework.context.ApplicationContext;
-//import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class Application {
@@ -14,6 +17,17 @@ public class Application {
     public static void main(String[] args) {
     	SpringApplication.run(Application.class, args);
 		System.out.println("Server started.");
+    }
+    
+    @SuppressWarnings("deprecation")
+	@Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/shuffle").allowedOrigins("*");
+            }
+        };
     }
 
 //    @Bean

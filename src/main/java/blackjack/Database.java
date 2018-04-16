c01a9ee004303c3b11f976f0df8b83fbbf0c881c
Aaron Ofengender <aofengen@gmail.com>
2018-04-16 11:53:22 -0400

    testing heroku database

--- src/main/java/blackjack/Database.java
+++ src/main/java/blackjack/Database.java
@@ -12,10 +12,11 @@ import java.util.Date;
 
 public class Database {
 
-//	public static void main(String[] args) throws Exception {
-////		createUserTable();
-//		insertIntoUserTable("Test", "x@x.com", "aofengen", "xxxxxxxx");
-//	}
+	public static void main(String[] args) throws Exception {
+//		createUserTable();
+		//insertIntoUserTable("Test", "x@x.com", "aofengen", "xxxxxxxx");
+		checkUserTable("a", "b");
+	}
 	
 	public static void createUserTable() {
 		Connection c = null;
@@ -90,25 +91,20 @@ public class Database {
 		
 	}
 
-	private static Connection getConnection() throws URISyntaxException, SQLException {
-	    String dbUrl = System.getenv("JDBC_DATABASE_URL");
-	    return DriverManager.getConnection(dbUrl);
-	}
-
 	public static void checkUserTable(String email, String password) {
 		Connection c = null;
 		Statement stmt = null;
 		try {
 			Class.forName("org.postgresql.Driver");
 			c = getConnection();
+			int id = 0;
 			System.out.println("Opened database successfully");	
-			
-			@SuppressWarnings("null")
-			ResultSet rs = stmt.executeQuery("SELECT FROM USERS WHERE email = '" + email + "' AND password = '" +
-							password + "';");
+			stmt = c.createStatement();
+			ResultSet rs = stmt.executeQuery("SELECT * FROM USERS WHERE email = 'x@x.com' AND password = 'xxxxxxxx';");
 			
 			if (rs.next()) {
-				System.out.println("Record Found!");
+				id = rs.getInt(1) + 1;
+				System.out.println("Record Found! " + id);
 			} else {
 				System.out.println("Record not found!");
 			}
@@ -119,5 +115,12 @@ public class Database {
 		System.out.println(e);
 	}
 }
+	
+	private static Connection getConnection() throws URISyntaxException, SQLException {
+	    String dbUrl = System.getenv("JDBC_DATABASE_URL");
+		return DriverManager.getConnection(dbUrl);
+//	    return DriverManager.getConnection("jdbc:postgresql://127.0.0.1:5432/blackjack", "postgres",
+//			"9074dewberry1136");
+	}
 
 }

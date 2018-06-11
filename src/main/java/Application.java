00d1164705b7cee91b91b9baaba11eb56bd17cae
Aaron Ofengender <aofengen@gmail.com>
2018-06-11 13:34:04 -0400

    testing api

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -5,7 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 //import org.springframework.web.bind.annotation.CrossOrigin;
 
 //@CrossOrigin(origins = "*")
-@SpringBootApplication(scanBasePackages={"controllers"})
+@SpringBootApplication
+//(scanBasePackages={"controllers"})
 public class Application {
 
     public static void main(String[] args) {
